79c3cfaff7931e11bdc69e4f11693999c533b0f6
Oleksiy Korobko <oleksiy_korobko@epam.com>
2016-03-15 01:04:28 -0400

    It seems like this is the frist working version.

---
+++ service/service/Areas/RouteDebugger/Models/ControllerSelectionInfo.cs
@@ -0,0 +1,9 @@
+namespace service.Areas.RouteDebugger.Models
+{
+    public class ControllerSelectionInfo
+    {
+        public string ControllerName { get; set; }
+
+        public string ControllerType { get; set; }
+    }
+}
